efe5c1495d1a3fdf1dbef3e2ae3b34fe8cf53a20
Guest User <student@epicodus.com>
2015-04-21 14:18:02 -0700

    tested with calculating A and Z into scores

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,4 +5,8 @@ describe('String#scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble_score()).to(eq(1))
   end
+
+  it("returns a scrabble score for a letter") do
+    expect("z".scrabble_score()).to(eq(10))
+  end
 end
