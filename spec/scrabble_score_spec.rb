048d01f1a90d40fdff92484cf1641729e832c6bb
Guest User <student@epicodus.com>
2015-04-21 14:58:39 -0700

    works for capital letters! thanks mike.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -13,4 +13,9 @@ describe('String#scrabble_score') do
   it("computes total scrabble score for a given word") do
     expect("quixotic".scrabble_score()).to(eq(26))
   end
+
+  it ("computes ANOTHER rockin' scrabble score in upcase") do
+    expect("DOG".scrabble_score()).to(eq(5))
+  end
+
 end
